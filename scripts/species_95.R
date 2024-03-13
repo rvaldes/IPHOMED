706b58fe3dcd6863841bf75041d779698f18eeac
rvaldes <rvaldesmas@gmail.com>
2024-03-13 20:31:26 +0200

    Updated species_95.R

--- scripts/species_95.R
+++ scripts/species_95.R
@@ -1,6 +1,6 @@
 
 ### Integrated Proteomics of HOst-MicrobiomE-Diet (IPHOMED)
-### Rafael Valdés-Mas
+### Rafael Valdés-Mas (Elinav Lab)
 
 ### Bacterial species abundances < 95%
 
@@ -12,17 +12,10 @@ library(readr)
 
 
 ## loading bacterial species taxIDs
-species_taxIDs <- read.table("automatic/bacteria-species.detailed.txt", header=FALSE, sep="\t", quote = "", check.names=FALSE, comment.char="")
+species_taxIDs <- read.table("automatic/bacteria-species.detailed.txt", header=FALSE, sep="\t", quote = "", check.names=FALSE, comment.char="") 
 colnames(species_taxIDs) <- c("TaxID", "Species", "Level")
-genbank_genomes <- read.table("assembly_summary.genbank.txt", header=TRUE, sep="\t", quote = "", check.names=FALSE, comment.char="", skip=1)
-refseq_genomes <- read.table("assembly_summary.refseq.txt", header=TRUE, sep="\t", quote = "", check.names=FALSE, comment.char="", skip=1)
-
-## select representative genomes
-taxIDs <- unique(refseq_genomes$taxid)
-for (i in 1:length(taxIDs)){
-	refseq_taxID <- subset( refseq_genomes, taxid == taxIDs[i] )
-	refseq_taxID %>% arrange(refseq_category, total_gene_count, genome_rep) %>% 
-}
+genbank_genomes <- read.table("assembly_summary.genbank.txt", header=TRUE, sep="\t", quote = "", check.names=FALSE, comment.char="", skip=1) 
+#refseq_genomes <- read.table("assembly_summary.refseq.txt", header=TRUE, sep="\t", quote = "", check.names=FALSE, comment.char="", skip=1)
 
 ## reading bracken outputs
 customized_read_tsv <- function(file){
@@ -61,7 +54,32 @@ candidates <- subset(counts, Cum_Percentage<=95)
 write.table(candidates, "bacteria.95.tsv", sep="\t", row.names=FALSE, quote=FALSE)
 
 ## selecting assembly_accession
-genomes <- subset(species_genomes, as.character(taxid) %in% as.character(candidates$TaxID) )
-final <- genomes %>% dplyr::distinct(ncbi_species_taxid, .keep_all = TRUE)
-write.table(final$ncbi_genbank_assembly_accession, "genomes.all.path", row.names=FALSE, sep="\t", quote=FALSE)
+genomes <- subset(genbank_genomes, as.character(species_taxid) %in% as.character(candidates$TaxID) )
+taxIDs <- unique(genomes$species_taxid)
+reference_genomes <- genomes[FALSE, ]
+for (i in 1:length(taxIDs)){
+    message(i)
+	genbank_taxID <- subset(genomes, species_taxid == taxIDs[i] )
+	if (length(subset(genbank_taxID, refseq_category=="reference genome" )$species_taxid) > 0){
+        genbank_taxID <- subset(genbank_taxID, refseq_category=="reference genome" ) %>% arrange(-as.numeric(protein_coding_gene_count))
+        reference_genomes <- rbind(reference_genomes, genbank_taxID[1,])
+    }else if (length(subset(genbank_taxID, refseq_category=="representative genome" )$species_taxid) > 0){
+        genbank_taxID <- subset(genbank_taxID, refseq_category=="representative genome" ) %>% arrange(-as.numeric(protein_coding_gene_count))
+        reference_genomes <- rbind(reference_genomes, genbank_taxID[1,])
+    }else if (length( subset(genbank_taxID, as.numeric(protein_coding_gene_count) > 0 & assembly_level=="Complete Genome")$species_taxid ) > 0){
+        genbank_taxID <- subset(genbank_taxID, as.numeric(protein_coding_gene_count) > 0 & assembly_level=="Complete Genome") %>% arrange(-as.numeric(protein_coding_gene_count))
+        reference_genomes <- rbind(reference_genomes, genbank_taxID[1,])
+    }else if (length( subset(genbank_taxID, as.numeric(protein_coding_gene_count) > 0 & genome_rep=="Full")$species_taxid ) > 0){
+        genbank_taxID <-  subset(genbank_taxID, as.numeric(protein_coding_gene_count) > 0 & genome_rep=="Full") %>% arrange(-as.numeric(protein_coding_gene_count))
+        reference_genomes <- rbind(reference_genomes, genbank_taxID[1,])
+    }else if (length( subset(genbank_taxID, as.numeric(protein_coding_gene_count) > 0)$species_taxid ) > 0){
+        genbank_taxID <- subset(genbank_taxID, as.numeric(protein_coding_gene_count) > 0) %>% arrange(-as.numeric(protein_coding_gene_count))
+        reference_genomes <- rbind(reference_genomes, genbank_taxID[1,])
+    }else{
+        genbank_taxID <- genbank_taxID %>% arrange(-as.numeric(genome_size))
+        reference_genomes <- rbind(reference_genomes, genbank_taxID[1,])
+    }
+}
 
+write.table(reference_genomes, "genomes.info.tsv", row.names=FALSE, sep="\t", quote=FALSE)
+write.table(reference_genomes$assembly_accession, "genome_assembly.tsv", row.names=FALSE, col.names = FALSE, sep="\t", quote=FALSE)
